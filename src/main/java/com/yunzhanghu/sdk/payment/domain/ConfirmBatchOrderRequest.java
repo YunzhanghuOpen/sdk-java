2adb63fd2fd63ad4be46a95c3cf879b5482e1a05
Boqh <qianhua.bo@yunzhanghu.com>
2022-10-18 19:24:20 +0800

    新增批量打款接口

---
+++ src/main/java/com/yunzhanghu/sdk/payment/domain/ConfirmBatchOrderRequest.java
@@ -0,0 +1,58 @@
+package com.yunzhanghu.sdk.payment.domain;
+
+
+
+
+// 批次确认请求
+public class ConfirmBatchOrderRequest {
+    
+    // 平台企业批次号
+    private String batchId;
+    
+    // 平台企业ID
+    private String dealerId;
+    
+    // 综合服务主体ID
+    private String brokerId;
+    
+    // 支付路径
+    private String channel;
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getBatchId() {
+        return batchId;
+    }
+    public void setDealerId(String dealerId) {
+        this.dealerId = dealerId;
+    }
+
+    public String getDealerId() {
+        return dealerId;
+    }
+    public void setBrokerId(String brokerId) {
+        this.brokerId = brokerId;
+    }
+
+    public String getBrokerId() {
+        return brokerId;
+    }
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    @Override
+    public String toString() {
+        return "ConfirmBatchOrderRequest{" +
+                ", batchId='" + batchId + '\'' +
+                ", dealerId='" + dealerId + '\'' +
+                ", brokerId='" + brokerId + '\'' +
+                ", channel='" + channel + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/yunzhanghu/sdk/payment/domain/CreateBatchOrderRequest.java
@@ -0,0 +1,102 @@
+package com.yunzhanghu.sdk.payment.domain;
+
+
+
+
+// 批量下单请求
+public class CreateBatchOrderRequest {
+    
+    // 平台企业批次号
+    private String batchId;
+    
+    // 平台企业ID
+    private String dealerId;
+    
+    // 综合服务主体ID
+    private String brokerId;
+    
+    // 支付路径
+    private String channel;
+    
+    // 平台企业的微信AppID
+    private String wxAppId;
+    
+    // 订单总金额
+    private String totalPay;
+    
+    // 总笔数
+    private String totalCount;
+    
+    // 订单列表
+    private BatchOrderInfo[] orderList;
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getBatchId() {
+        return batchId;
+    }
+    public void setDealerId(String dealerId) {
+        this.dealerId = dealerId;
+    }
+
+    public String getDealerId() {
+        return dealerId;
+    }
+    public void setBrokerId(String brokerId) {
+        this.brokerId = brokerId;
+    }
+
+    public String getBrokerId() {
+        return brokerId;
+    }
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+    public void setWxAppId(String wxAppId) {
+        this.wxAppId = wxAppId;
+    }
+
+    public String getWxAppId() {
+        return wxAppId;
+    }
+    public void setTotalPay(String totalPay) {
+        this.totalPay = totalPay;
+    }
+
+    public String getTotalPay() {
+        return totalPay;
+    }
+    public void setTotalCount(String totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public String getTotalCount() {
+        return totalCount;
+    }
+    public void setOrderList(BatchOrderInfo[] orderList) {
+        this.orderList = orderList;
+    }
+
+    public BatchOrderInfo[] getOrderList() {
+        return orderList;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateBatchOrderRequest{" +
+                ", batchId='" + batchId + '\'' +
+                ", dealerId='" + dealerId + '\'' +
+                ", brokerId='" + brokerId + '\'' +
+                ", channel='" + channel + '\'' +
+                ", wxAppId='" + wxAppId + '\'' +
+                ", totalPay='" + totalPay + '\'' +
+                ", totalCount='" + totalCount + '\'' +
+                ", orderList='" + orderList + '\'' +
+                '}';
+    }
+}
