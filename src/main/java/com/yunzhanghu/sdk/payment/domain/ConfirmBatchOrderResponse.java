0e0c3c4fa4121d4af6621b52ab2e6f1a6c41b623
Boqh <qianhua.bo@yunzhanghu.com>
2022-10-27 15:29:46 +0800

    删除多余的包

--- src/main/java/com/yunzhanghu/sdk/payment/domain/ConfirmBatchOrderResponse.java
+++ src/main/java/com/yunzhanghu/sdk/payment/domain/ConfirmBatchOrderResponse.java
@@ -2,14 +2,14 @@ package com.yunzhanghu.sdk.payment.domain;
 
 
 
-import java.util.List;
 
 // 批次确认响应
 public class ConfirmBatchOrderResponse {
 
+
     @Override
-    public String toString() {
-        return "ConfirmBatchOrderResponse{" +
-           '}';
-    }
+  public String toString() {
+      return "ConfirmBatchOrderResponse{" +
+         '}';
+  }
 }
